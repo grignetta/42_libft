e12d6252b04df5259ca160effd5e00b58152bc83
grignetta <121875111+grignetta@users.noreply.github.com>
2025-08-22 17:07:51 +0200

    libft.h added strtok

--- libft.h
+++ libft.h
@@ -51,5 +51,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+char	*ft_strtok(char *str, const char *delim);
 
 #endif
